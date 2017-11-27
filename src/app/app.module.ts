869485655c88dc94bca83f988239dfa57c3f4754
Kamran Ahmed <kamranahmed.se@gmail.com>
2017-11-27 20:08:30 +0400

    Featured post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { routing } from './app.routes';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { FeaturedPostComponent } from './components/featured-post/featured-post.component';
 
 @NgModule(<NgModule>{
   declarations: [
     AppComponent,
     HomeComponent,
     TopNavComponent,
+    FeaturedPostComponent,
   ],
   imports: [
     NgbModule.forRoot(),
